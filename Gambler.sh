ed2b2b5387db9090a2ba30dfbfe2a7d3c3d25ecf
gauravm-bit <gauravwrk01@gmail.com>
2020-03-09 11:46:53 +0530

    ADDED:fifty percent stake

--- Gambler.sh
+++ Gambler.sh
@@ -5,16 +5,24 @@ echo "Welcome to Gambler Simulation problem"
 #CONSTANTS
 STAKE=100
 BET=1
+FIFTYPERCENTAGE=$(($STAKE*50/100))
+MINIMUMSTAKE=$(($STAKE - $FIFTYPERCENTAGE))
+MAXIMUMSTAKE=$(($STAKE + $FIFTYPERCENTAGE))
 
 #variables
 currentStake=STAKE
+bets=0
 
 #Win or loose
-randomCheck=$(( RANDOM%2 ))
-if (( randomCheck == 0 ))
-then
-	currentStake=$(($currentStake + $BET))
-else
-	currentStake=$(($currentStake - $BET))
-fi
- 
+
+while [[ currentStake -gt MINIMUMSTAKE &&
+			 currentStake -lt MAXIMUMSTAKE ]]
+do
+	randomCheck=$(( RANDOM%2 ))
+	if (( $randomCheck == 0 ))
+	then
+		currentStake=$(($currentStake + $BET))
+	else
+		currentStake=$(($currentStake - $BET))
+	fi
+done
