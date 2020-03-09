95278c6fda24934e46326edba5f88260bbadc0fd
gauravm-bit <gauravwrk01@gmail.com>
2020-03-09 11:09:27 +0530

    ADDED:win or loose

--- Gambler.sh
+++ Gambler.sh
@@ -5,3 +5,16 @@ echo "Welcome to Gambler Simulation problem"
 #CONSTANTS
 STAKE=100
 BET=1
+
+#variables
+currentStake=STAKE
+
+#Win or loose
+randomCheck=$(( RANDOM%2 ))
+if (( randomCheck == 0 ))
+then
+	currentStake=$(($currentStake + $BET))
+else
+	currentStake=$(($currentStake - $BET))
+fi
+ 
