527ca388811d537a7df7b977a529c837c7014954
gauravm-bit <gauravwrk01@gmail.com>
2020-03-09 15:49:07 +0530

    ADDED:monthly win or lose

--- Gambler.sh
+++ Gambler.sh
@@ -22,33 +22,33 @@ result=""
 
 for(( index = 1; index <=3; index++))
 do
-	for(( i = 1; i <= 20; i++ ))
-	do
-		currentStake=$STAKE
-		while [[ currentStake -gt MINIMUMSTAKE &&
-					 currentStake -lt MAXIMUMSTAKE ]]
-		do
-			randomCheck=$(( RANDOM%2 ))
-			if (( $randomCheck == 0 ))
-			then
-				currentStake=$(($currentStake + $BET))
-			else
-				currentStake=$(($currentStake - $BET))
-			fi
-		done
-		if (( $currentStake == MAXIMUMSTAKE ))
-		then
-			((wins++))
-			monthlystake=$(( $monthlystake + $FIFTYPERCENTAGE ))
-			result="win"
-		else
-			((loss++))
-			mothlystake=$(( $monthlystake + $FIFTYPERCENTAGE ))
-			result="loose"
-		fi
-	((day++))
-	echo "$day day $FIFTYPERCENTAGE $result"
-	done
+   for(( i = 1; i <= 20; i++ ))
+   do
+      currentStake=$STAKE
+      while [[ currentStake -gt MINIMUMSTAKE &&
+                currentStake -lt MAXIMUMSTAKE ]]
+      do
+         randomCheck=$(( RANDOM%2 ))
+         if (( $randomCheck == 0 ))
+         then
+            currentStake=$(($currentStake + $BET))
+         else
+            currentStake=$(($currentStake - $BET))
+         fi
+      done
+      if (( $currentStake == MAXIMUMSTAKE ))
+      then
+         ((wins++))
+         monthlystake=$(( $monthlystake + $FIFTYPERCENTAGE ))
+         result="win"
+      else
+         ((loss++))
+         mothlystake=$(( $monthlystake + $FIFTYPERCENTAGE ))
+         result="loose"
+      fi
+		((day++))
+   	echo "$day day $FIFTYPERCENTAGE $result"
+   done
 TotalWins=$(($wins * $FIFTYPERCENTAGE))
 TotalLosses=$(($loss * $FIFTYPERCENTAGE))
 echo "Total win for the month "$TotalWins
@@ -58,3 +58,4 @@ win=0
 loss=0
 day=0
 done
+
